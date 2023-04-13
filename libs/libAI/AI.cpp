98cadd8cab4090ed4e45889161c32cc13933f14c
Степан <31482802+gh0st17@users.noreply.github.com>
2023-04-13 22:35:25 +0300

    Улучшение кода

--- libs/libAI/AI.cpp
+++ libs/libAI/AI.cpp
@@ -6,37 +6,33 @@
 * @return Ход компьютера
 */
 unsigned short generate_move(Field field) {
-  unsigned short move = 10;
+  unsigned short move = 9;
 
   // Проверяем, есть ли выигрышные ходы
-  for (unsigned short i = 0; i < 9; ++i)
+  for (unsigned short i = 0; i < 9 && move > 8; ++i)
     if (field[i] == Cell::Unused) {
       field[i] = Cell::X; // Предполагаемый ход компьютера
 
-      if (check_win_state()) {
+      if (check_win_state())
         move = i;
-        field[i] = Cell::Unused; // Отменяем предполагаемый ход компьютера
-        break;
-      }
+
       field[i] = Cell::Unused; // Отменяем предполагаемый ход компьютера
     }
 
   // Если нет выигрышных ходов, блокируем ходы соперника
-  if (move == 10)
-    for (int i = 0; i < 9; ++i)
+  if (move == 9)
+    for (int i = 0; i < 9 && move > 8; ++i)
       if (field[i] == Cell::Unused) {
         field[i] = Cell::O; // Предполагаемый ход соперника
         
-        if (check_win_state()) {
+        if (check_win_state())
           move = i;
-          field[i] = Cell::Unused; // Отменяем предполагаемый ход соперника
-          break;
-        }
+
         field[i] = Cell::Unused; // Отменяем предполагаемый ход соперника
       }
 
   // Если нет выигрышных и блокирующих ходов, выбираем случайный ход
-  if (move == 10)
+  if (move == 9)
     move = rand() % 9;
 
   return move;
