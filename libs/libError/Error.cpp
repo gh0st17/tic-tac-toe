c295215a1abbe50166f6a34598d7fb1bf02ca0d1
Степан <31482802+gh0st17@users.noreply.github.com>
2023-04-21 15:18:51 +0300

    Замена кодов ошибок на ошибки перечисления

--- libs/libError/Error.cpp
+++ libs/libError/Error.cpp
@@ -1,32 +1,28 @@
 #include "Error.hpp"
 
-void game_error_code(int code) {
-  if (code == 1)
+void game_error_code(GameError code) {
+  if (code == GameError::CANT_MOVE_HERE)
     print_error("Can't move here");
-  else if (code == 2)
-    print_error("It is not a digit");
-  else if (code == 3)
+  else if (code == GameError::INCORRECT_INPUT)
     print_error("Incorrect input");
   else
     print_error("Unknown error");
 }
 
-void test_error_code(int code) {
-  if (code == 1)
-    print_error("Selection player mode failed");
-  else if (code == 2)
+void test_error_code(TestError code) {
+  if (code == TestError::GET_NON_ZERO)
     print_error("Getting non-zero value failed");
-  else if (code == 3)
+  else if (code == TestError::DETECT_WINNER)
     print_error("Detecting winner state failed");
-  else if (code == 4)
+  else if (code == TestError::CHK_EMPTY_CELL)
     print_error("Check empty cell failed");
-  else if (code == 5)
+  else if (code == TestError::CHK_USED_CELL)
     print_error("Check used cell failed");
-  else if (code == 6)
+  else if (code == TestError::CHK_RND_CELL)
     print_error("Check random cell failed");
-  else if (code == 7)
+  else if (code == TestError::COMPUTER_STEP)
     print_error("Computer step failed");
-  else if (code == 8)
+  else if (code == TestError::ASSET_FILE)
     print_error("Can't open assets file");
   else
     print_error("Unknown error");
